0d4f8f24c83f9919d90590d016e8af2f82ea9ab7
avalon <2976084714@qq.com>
2021-07-12 11:57:13 +0800

    'update'

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
+const path = require('path')
+
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
+
 module.exports = {
   outputDir: 'docs',
-  publicPath: '/workspace/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/workspace/' : '/',
+  lintOnSave: true,
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('assets', resolve('src/assets'))
+      .set('pages', resolve('src/pages'))
+  }
 }
